5b65bdf3c4dcac2f3e97813a323ef312031d3a93
meirozenfeld <meirozenfeld@gmail.com>
2019-12-02 16:37:08 +0200

    Adding empty junit Polynom (check first commit)

---
+++ src/myMath/PolynomTest.java
@@ -0,0 +1,99 @@
+package myMath;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PolynomTest {
+
+	@Test
+	void testPolynom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPolynomString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testF() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddPolynom_able() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMonom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubstract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplyPolynom_able() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsZero() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCopy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDerivative() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIteretor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplyMonom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPolynomOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInitFromString() {
+		fail("Not yet implemented");
+	}
+
+}
