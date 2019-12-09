5c2e35ca3aca2e23494ca53f7e62457b82d67000
meirozenfeld <meirozenfeld@gmail.com>
2019-12-09 19:03:19 +0200

    add initfromstring

--- src/myMath/ComplexFunction.java
+++ src/myMath/ComplexFunction.java
@@ -12,7 +12,12 @@ public class ComplexFunction implements complex_function {
 		this.right = right;
 		this.operation = operation;
 	}
-
+	public ComplexFunction()
+	{
+		this.left =null;
+		this.right = null;
+		this.operation = Operation.None;	
+	}
 
 
 	public ComplexFunction(function f)
@@ -27,9 +32,10 @@ public class ComplexFunction implements complex_function {
 		this.left = left;
 		this.right = right;
 		this.operation=Operation.valueOf(operation);
+//		if(this.operation.name()=="None")this.setOp(Operation.valueOf("Comp"));
 		if(this.operation.name()=="Error")throw new RuntimeException("Illegal Operation");
 
-		
+
 	}
 
 
@@ -69,7 +75,7 @@ public class ComplexFunction implements complex_function {
 			return this.left().f(x);
 		case "Error":
 			throw new RuntimeException("Illegal Operation");
-			
+
 
 		default: //case "Error"
 			throw new RuntimeException("Illegal Operation");
@@ -79,8 +85,26 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public function initFromString(String s) {
-
-		return null;
+		function f;
+		s=removeSpace(s);
+		if(s.indexOf('(')==-1&&s.indexOf(')')==-1)//base case- without operator and complex function (just Monom or Polynom)
+		{
+			Polynom p=new Polynom(s);
+			function l=p;
+			return l.initFromString(s);
+		}
+		else
+		{
+			int start=s.indexOf('(');// index of open '('
+			int end=s.indexOf(')');// index of close ')'
+			String op = s.substring(0, start); // save operation
+			s=s.substring(start+1,end);
+			int comma=s.indexOf(','); // index of ','
+			function l=initFromString(s.substring(0, comma)); // recursive call of left function
+			function r=initFromString(s.substring(comma+1)); // recurcive call of right function
+			f=f= new ComplexFunction(op,l,r);
+		}
+				return f;
 	}
 
 	@Override
@@ -111,8 +135,8 @@ public class ComplexFunction implements complex_function {
 		}
 		else
 		{
-		this.right=f1.copy();
-		this.setOp(Operation.valueOf("Plus"));
+			this.right=f1.copy();
+			this.setOp(Operation.valueOf("Plus"));
 		}
 	}
 
@@ -120,7 +144,7 @@ public class ComplexFunction implements complex_function {
 	public void mul(function f1) {
 
 
-	ComplexFunction temp;
+		ComplexFunction temp;
 		if(this.right!= null)
 		{
 			temp = new ComplexFunction (this.operation,this.left.copy(),this.right.copy());
@@ -260,33 +284,42 @@ public class ComplexFunction implements complex_function {
 		}
 		return s.toString();
 	}
+	private String removeSpace(String str) 
+	{ 
+		str = str.replaceAll("\\s",""); 
+		return str; 
+	} 
 
 
 
 	public static void main(String[] args) {
 
 		Polynom p3 = new Polynom("x+3");
-		ComplexFunction cf3 = new ComplexFunction("Error",p3,p3);
-//		System.out.println("1:"+cf3);
-//		ComplexFunction cf4 = new ComplexFunction("Plus", p3,p3); 
-//		System.out.println("2:"+cf4);
-//		cf3.plus(cf4);
-//		System.out.println("3:"+cf3);
-//		cf3.plus(cf4);
-//		System.out.println("4:"+cf3);//not good..need to check
-		System.out.println(cf3);
-//		cf3.plus(cf3);
+//		ComplexFunction cf3 = new ComplexFunction("Error",p3,p3);
+		//		System.out.println("1:"+cf3);
+		//		ComplexFunction cf4 = new ComplexFunction("Plus", p3,p3); 
+		//		System.out.println("2:"+cf4);
+		//		cf3.plus(cf4);
+		//		System.out.println("3:"+cf3);
+		//		cf3.plus(cf4);
+		//		System.out.println("4:"+cf3);//not good..need to check
 //		System.out.println(cf3);
-		
-		
-//		System.out.println(cf4);
-//		cf4.plus(cf4);
-//		cf4.mul(cf4);
-//		cf4.div(cf4);
-//		cf4.min(cf4);
-//		cf4.max(cf4);
-//	ComplexFunction cf5=new ComplexFunction("Times",p3,p3);
-//			System.out.println(cf5);
+		//		cf3.plus(cf3);
+		//		System.out.println(cf3);
+
+
+		//		System.out.println(cf4);
+		//		cf4.plus(cf4);
+		//		cf4.mul(cf4);
+		//		cf4.div(cf4);
+		//		cf4.min(cf4);
+		//		cf4.max(cf4);
+		//	ComplexFunction cf5=new ComplexFunction("Times",p3,p3);
+		//			System.out.println(cf5);
+            function cff = new ComplexFunction();
+            System.out.println(cff);
+           function cff2 = cff.initFromString("Plus(x,2x)");
+           System.out.println(cff2);
 
 	}
 
