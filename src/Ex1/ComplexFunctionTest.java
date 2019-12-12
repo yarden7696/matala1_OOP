a76bf978442dbf5197429479ae9fe3172cda91aa
meirozenfeld <meirozenfeld@gmail.com>
2019-12-12 10:29:51 +0200

    finish ComplexFunctionTest

--- src/Ex1/ComplexFunctionTest.java
+++ src/Ex1/ComplexFunctionTest.java
@@ -16,10 +16,10 @@ class ComplexFunctionTest {
 		for (int i = 0; i < 1; i++) 
 		{
 			String opi=op[i];
-				Polynom l=new Polynom(polynomLeft[0]);
-				Polynom r=new Polynom(polynomRight[0]);
-				ComplexFunction actual=new ComplexFunction(opi,l,r);
-				assertEquals(expected[i], actual.toString());
+			Polynom l=new Polynom(polynomLeft[0]);
+			Polynom r=new Polynom(polynomRight[0]);
+			ComplexFunction actual=new ComplexFunction(opi,l,r);
+			assertEquals(expected[i], actual.toString());
 		}
 		String [] wrongOp= {"Times","none","+"}; // none refer to none on 2 functions
 		for (int i = 0; i < wrongOp.length; i++) 
@@ -28,7 +28,7 @@ class ComplexFunctionTest {
 			Polynom r=new Polynom(polynomRight[0]);
 			try
 			{
-			
+
 				ComplexFunction wrongCf=new ComplexFunction(wrongOp[i],l,r);
 				fail();
 			}
@@ -41,12 +41,12 @@ class ComplexFunctionTest {
 
 	@Test
 	void testF() {
-		String [] op = {"plus","mul","plus"};
-		String [] polynomLeft= {"2x^2-x+1.5","1.2x+1","5x^10-4x+7"};
-		String [] polynomRight= {"-x^4","4x^3+5-0","-5x^10+4x-7"};
-		double [] expected= {-64.5,519.8,0.0};
+		String [] op = {"plus","mul","plus","div","max","min","comp"};
+		String [] polynomLeft= {"2x^2-x+1.5","1.2x+1","5x^10-4x+7","4x^2+x-1","2x^2-2x","-4x^2","2x+2.2"};
+		String [] polynomRight= {"-x^4","4x^3+5-0","-5x^10+4x-7","2x^2+2","2x^3-2x+1","-11.88x","2.2x^2-2"};
+		double [] expected= {-64.5,519.8,0.0,1.9,49.0,-36.0,37.800000000000004};
 		for (int i = 0; i < expected.length; i++) 
-		{
+		{	
 			Polynom l=new Polynom(polynomLeft[i]);
 			Polynom r=new Polynom(polynomRight[i]);
 			String opi=op[i];
@@ -54,14 +54,16 @@ class ComplexFunctionTest {
 			double actual=cf.f(3);
 			assertEquals(expected[i], actual);
 		}
+
+
 	}
 
 	@Test
 	void testInitFromString() {
-		
+
 		String [] polynomLeft= {"2x^2-x+1.5","-1.2x+1"};
 		String [] polynomRight= {"-x^4","4x^3"};
-		
+
 		Polynom l=new Polynom(polynomLeft[0]);
 		Polynom r=new Polynom(polynomRight[0]);
 		ComplexFunction cf=new ComplexFunction("Plus",l,r);
@@ -136,7 +138,7 @@ class ComplexFunctionTest {
 		String [] polynomRight= {"-2x^2+x-1.5","4x^3","x^4-2.3"};
 		String [] expected= {"div(div(2.0x^2-1.0x+1.5,-2.0x^2+1.0x-1.5),div(div(2.0x^2-1.0x+1.5,-2.0x^2+1.0x-1.5),-2.0x^2+1.0x-1.5))",
 				"div(div(2.0x^3-1.2x+1.0,4.0x^3),div(div(2.0x^3-1.2x+1.0,4.0x^3),4.0x^3))",
-				"div(div(1.0x^4-5.3,1.0x^4-2.3),div(div(1.0x^4-5.3,1.0x^4-2.3),1.0x^4-2.3))"};
+		"div(div(1.0x^4-5.3,1.0x^4-2.3),div(div(1.0x^4-5.3,1.0x^4-2.3),1.0x^4-2.3))"};
 		for (int i = 0; i < expected.length; i++) 
 		{
 			Polynom l=new Polynom(polynomLeft[i]);
@@ -153,8 +155,8 @@ class ComplexFunctionTest {
 		String [] polynomLeft= {"2x^2-x+1.5","2x^3-1.2x+1","x^4-5.3"};
 		String [] polynomRight= {"-2x^2+x-1.5","4x^3","x^4-2.3"};
 		String [] expected= {"max(max(2.0x^2-1.0x+1.5,-2.0x^2+1.0x-1.5),max(max(2.0x^2-1.0x+1.5,-2.0x^2+1.0x-1.5),-2.0x^2+1.0x-1.5))",
-			"max(max(2.0x^3-1.2x+1.0,4.0x^3),max(max(2.0x^3-1.2x+1.0,4.0x^3),4.0x^3))",
-				"max(max(1.0x^4-5.3,1.0x^4-2.3),max(max(1.0x^4-5.3,1.0x^4-2.3),1.0x^4-2.3))"};
+				"max(max(2.0x^3-1.2x+1.0,4.0x^3),max(max(2.0x^3-1.2x+1.0,4.0x^3),4.0x^3))",
+		"max(max(1.0x^4-5.3,1.0x^4-2.3),max(max(1.0x^4-5.3,1.0x^4-2.3),1.0x^4-2.3))"};
 		for (int i = 0; i < expected.length; i++) 
 		{
 			Polynom l=new Polynom(polynomLeft[i]);
@@ -171,8 +173,8 @@ class ComplexFunctionTest {
 		String [] polynomLeft= {"2x^2-x+1.5","2x^3-1.2x+1","x^4-5.3"};
 		String [] polynomRight= {"-2x^2+x-1.5","4x^3","x^4-2.3"};
 		String [] expected= {"min(min(2.0x^2-1.0x+1.5,-2.0x^2+1.0x-1.5),min(min(2.0x^2-1.0x+1.5,-2.0x^2+1.0x-1.5),-2.0x^2+1.0x-1.5))",
-			"min(min(2.0x^3-1.2x+1.0,4.0x^3),min(min(2.0x^3-1.2x+1.0,4.0x^3),4.0x^3))",
-				"min(min(1.0x^4-5.3,1.0x^4-2.3),min(min(1.0x^4-5.3,1.0x^4-2.3),1.0x^4-2.3))"};
+				"min(min(2.0x^3-1.2x+1.0,4.0x^3),min(min(2.0x^3-1.2x+1.0,4.0x^3),4.0x^3))",
+		"min(min(1.0x^4-5.3,1.0x^4-2.3),min(min(1.0x^4-5.3,1.0x^4-2.3),1.0x^4-2.3))"};
 		for (int i = 0; i < expected.length; i++) 
 		{
 			Polynom l=new Polynom(polynomLeft[i]);
@@ -202,6 +204,46 @@ class ComplexFunctionTest {
 
 	@Test
 	void testEqualsObject() {
-		fail("Not yet implemented");
+		String [] op = {"plus","mul","plus","div","comp"};
+		String [] polynomLeft= {"4x^2-2.3","-5x+0.5","-x+x+2x^4","66x^1+2x","2x^3-2x^2-2.1x"};
+		String [] polynomRight= {"3","2x+1","-2x-3.5","3.2x^2","0"};
+		
+		for (int i = 0; i < polynomLeft.length; i+=2) 
+		{	
+			Polynom l=new Polynom(polynomLeft[i]);
+			Polynom r=new Polynom(polynomRight[i]);
+			String opi=op[i];
+			ComplexFunction actual=new ComplexFunction(opi,l,r);
+			ComplexFunction expected=new ComplexFunction(opi,l,r);
+			if(!actual.equals(expected))
+			{
+				fail("The objects are equals");
+			}
+			function f=new ComplexFunction(opi,l,r);
+			actual.mul(f);
+			expected.mul(f);
+			if(!actual.equals(expected))
+			{
+				fail("The objects are equals");
+			}
+			actual.div(f);
+			expected.div(f);
+			if(!actual.equals(expected))
+			{
+				fail("The objects are equals");
+			}
+			actual.max(f);
+			expected.max(f);
+			if(!actual.equals(expected))
+			{
+				fail("The objects are equals");
+			}
+			actual.comp(f);
+			expected.comp(f);
+			if(!actual.equals(expected))
+			{
+				fail("The objects are equals");
+			}
+		}
 	}
 }
