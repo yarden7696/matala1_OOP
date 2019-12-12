09f2ee0333d826b354fed07baf4fa678cd3ef999
meirozenfeld <meirozenfeld@gmail.com>
2019-12-12 16:51:59 +0200

    ComplexFuction: adding notes and arrange rows

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,13 +1,17 @@
 package Ex1;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ComplexFunction implements complex_function {
-
+	
 	private function left;
 	private function right;
 	private Operation operation;
 	public static final double EPSILON = 0.0000001; // for equals
+	/**
+	 * Constractor for operation, function left and right
+	 * @param operation
+	 * @param left
+	 * @param right
+	 */
 	public ComplexFunction(Operation operation,function left, function right)
 	{
 		this.left = left.copy();
@@ -15,13 +19,12 @@ public class ComplexFunction implements complex_function {
 		this.operation = operation;
 	}
 	//	defoult constractor to test initfromstring
-//	public ComplexFunction()
-//	{
-//		this.left =null;
-//		this.right = null;
-//		this.operation = Operation.None;	
-//	}
-
+	//	public ComplexFunction()
+	//	{
+	//		this.left =null;
+	//		this.right = null;
+	//		this.operation = Operation.None;	
+	//	}
 
 	public ComplexFunction(function f)
 	{
@@ -29,13 +32,17 @@ public class ComplexFunction implements complex_function {
 		this.right = null;
 		this.operation = Operation.None;	
 	}
-
+	/**
+	 * constractor for operation(String), function left and right
+	 * @param operation
+	 * @param left
+	 * @param right
+	 */
 	public ComplexFunction(String operation,function left, function right)
 	{	
 		if(left!=null)this.left = left.copy();
 		if(right!=null)this.right = right.copy();
 		//		this.operation=Operation.valueOf(operation);
-
 		switch(operation.toLowerCase())
 		{
 		case "plus":
@@ -69,8 +76,10 @@ public class ComplexFunction implements complex_function {
 		}
 	}
 
-
 	@Override
+	/**
+	 * Calculate f(x) with operation and return the result
+	 */
 	public double f(double x) {
 
 		String  opera = this.operation.name();
@@ -78,43 +87,37 @@ public class ComplexFunction implements complex_function {
 		{
 		case "Plus" :
 			return this.left().f(x)+ this.right().f(x);
-
 		case "Times" :
 			return this.left().f(x) * this.right().f(x);
-
 		case "Divid" :
 			return this.left().f(x) / this.right().f(x);
-
 		case "Max" :
 			if (this.left().f(x) > this.right().f(x))	{return this.left().f(x);}
 			else
 			{
 				return this.right().f(x);
 			}
-
 		case "Min" :
 			if (this.left().f(x) < this.right().f(x)) {return this.left().f(x);}
 			else 
 			{
 				return this.right().f(x);
 			}
-
 		case "Comp" :
 			return this.left().f( this.right().f(x));
-
 		case "None" :
 			return this.left().f(x);
 		case "Error":
 			throw new RuntimeException("Illegal Operation");
-
-
 		default: 
 			throw new RuntimeException("Illegal Operation");
-
 		}
 	}
 
 	@Override
+	/**
+	 *  Get a string function and return function
+	 */
 	public function initFromString(String s) {
 		function f;
 		s=removeSpace(s);
@@ -136,12 +139,14 @@ public class ComplexFunction implements complex_function {
 		}
 		return f;
 	}
-	// help function to find the comma between left and right
+	
+	/**
+	 * help function to find the comma between left and right
+	 */
 	public int findComma(String s,int start){
 		int iComma=0; // index of comma
 		int open=1; //sum of '('
 		int i=start+1; // start after first '('
-
 		while(i<s.length()&&open!=iComma) 
 		{
 			if(s.charAt(i)=='(')open++;
@@ -158,7 +163,6 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public function copy() {
-
 		ComplexFunction temp;
 		if(this.right== null)
 		{
@@ -191,8 +195,6 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void mul(function f1) {
-
-
 		ComplexFunction temp;
 		if(this.right!= null)
 		{
@@ -210,12 +212,10 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void div(function f1) {
-
 		ComplexFunction temp;
 		if(this.right!= null)
 		{
 			temp = new ComplexFunction (this.operation,this.left.copy(),this.right.copy());
-
 			this.setLeft(temp);
 			this.setRight(f1.copy());
 			this.setOp(Operation.valueOf("Divid"));
@@ -229,12 +229,10 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void max(function f1) {
-
 		ComplexFunction temp;
 		if(this.right!= null)
 		{
 			temp = new ComplexFunction (this.operation,this.left.copy(),this.right.copy());
-
 			this.setLeft(temp);
 			this.setRight(f1.copy());
 			this.setOp(Operation.valueOf("Max"));
@@ -248,12 +246,10 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void min(function f1) {
-
 		ComplexFunction temp;
 		if(this.right!= null)
 		{
 			temp = new ComplexFunction (this.operation,this.left.copy(),this.right.copy());
-
 			this.setLeft(temp);
 			this.setRight(f1.copy());
 			this.setOp(Operation.valueOf("Min"));
@@ -267,12 +263,10 @@ public class ComplexFunction implements complex_function {
 
 	@Override
 	public void comp(function f1) {
-
 		ComplexFunction temp;
 		if(this.right!= null)
 		{
 			temp = new ComplexFunction (this.operation,this.left.copy(),this.right.copy());
-
 			this.setLeft(temp);
 			this.setRight(f1.copy());
 			this.setOp(Operation.valueOf("Comp"));
@@ -287,7 +281,6 @@ public class ComplexFunction implements complex_function {
 	@Override
 	public boolean equals(Object obj) {
 		boolean flag=true;
-
 		if(obj instanceof ComplexFunction) // if object is Complexfunction type
 		{
 			ComplexFunction cf= (ComplexFunction) obj;
@@ -307,38 +300,32 @@ public class ComplexFunction implements complex_function {
 		return this.left;
 	}
 
-
 	@Override
 	public function right() 
 	{
 		return this.right;
 	}
 
-
 	public void setRight(function f) 
 	{
 		this.right= f;
 	}
-
 
 	public void setLeft(function f) 
 	{
 		this.left= f;
 	}
 
-
 	public void setOp(Operation op) 
 	{
 		this.operation= op;
 	}
-
 
 	@Override
 	public Operation getOp() 
 	{
 		return this.operation;
 	}
-
 
 	public String toString() {
 		StringBuilder s = new StringBuilder();
@@ -378,7 +365,6 @@ public class ComplexFunction implements complex_function {
 		{
 			s.append(this.right);
 		}
-
 		return s.toString();
 	}
 	private String removeSpace(String str) 
@@ -386,63 +372,4 @@ public class ComplexFunction implements complex_function {
 		str = str.replaceAll("\\s",""); 
 		return str; 
 	} 
-
-
-
-	public static void main(String[] args) {
-
-//		Polynom p3 = new Polynom("x+1");
-//		Polynom p4 = new Polynom("x+1");
-//		ComplexFunction cf3 = new ComplexFunction("plus",p3,p3);
-//		ComplexFunction cf5 = new ComplexFunction("plus",p3,p4);
-//		System.out.println(cf3.equals(cf5));
-//						System.out.println("1:"+cf3);
-//						
-//						ComplexFunction cf4 = new ComplexFunction("mul", p3,p3); 
-//						System.out.println("2:"+cf4);
-		//				cf3.plus(cf4);
-		//				System.out.println("3:"+cf3);
-		//				cf3.plus(cf4);
-		//				System.out.println("4:"+cf3);//not good..need to check
-		//		System.out.println(cf3);
-		//		cf3.plus(cf3);
-		//		System.out.println(cf3);
-
-
-		//		System.out.println(cf4);
-		//		cf4.plus(cf4);
-		//		cf4.mul(cf4);
-		//		cf4.div(cf4);
-		//		cf4.min(cf4);
-		//		cf4.max(cf4);
-		//	ComplexFunction cf5=new ComplexFunction("Times",p3,p3);
-		//			System.out.println(cf5);
-		//		function cff = new ComplexFunction();
-		//		System.out.println(cff);
-		//		function cff2 = cff.initFromString("div(plus(1,2),3)");
-		//		System.out.println(cff2);
-//						String [] polynomLeft= {"2x^2-x+1.5","-1.2x+1","5"};
-//						String [] polynomRight= {"-x^4","4x^3","5.2x+1"};
-//						Polynom l=new Polynom(polynomLeft[0]);
-//						Polynom r=new Polynom(polynomRight[0]);
-//						ComplexFunction cf=new ComplexFunction("Plus",l,r);
-//						function cf1=cf.initFromString("plus(2x^2-x+1.5,-x^4)");
-//						assertEquals(cf, cf1);
-//						cf.mul(cf1);
-//						cf1=cf.initFromString("mul(plus(2x^2-x+1.5,-x^4),plus(2x^2-x+1.5,-x^4))");
-//						assertEquals(cf, cf1);
-//						cf.div(cf1);
-//						System.out.println(cf);
-		String [] polynomLeft= {"2x^2-x+1.5","2x^3-1.2x+1","x^4-5.3"};
-		String [] polynomRight= {"-2x^2+x-1.5","4x^3","x^4-2.3"};
-		for (int i = 0; i < polynomLeft.length; i++) 
-		{
-			Polynom l=new Polynom(polynomLeft[i]);
-			Polynom r=new Polynom(polynomRight[i]);
-			ComplexFunction cf=new ComplexFunction("max",l,r);
-			cf.max(cf);
-			System.out.println(cf+",");
-		}
-	}
-
 }
